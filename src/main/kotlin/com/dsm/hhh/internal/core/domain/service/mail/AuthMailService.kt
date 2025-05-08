ce1f8157a086cea6357a9057a38b09337fca076f
eeuunn <haspring@dsm.hs.kr>
2025-05-08 09:02:51 +0900

    chore : 서비스 수정

--- src/main/kotlin/com/dsm/hhh/internal/core/domain/service/mail/AuthMailService.kt
+++ src/main/kotlin/com/dsm/hhh/internal/core/domain/service/mail/AuthMailService.kt
@@ -2,7 +2,7 @@ package com.dsm.hhh.internal.core.domain.service.mail
 
 import com.dsm.hhh.internal.core.domain.model.dto.mail.AuthMailDTO
 import com.dsm.hhh.internal.core.domain.model.dto.mail.VerifyCodeDTO
-import com.dsm.hhh.internal.core.usecase.AuthMailUseCase
+import com.dsm.hhh.internal.core.usecase.mail.AuthMailUseCase
 import com.dsm.hhh.internal.data.repository.mail.AuthMailRepository
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
@@ -14,17 +14,22 @@ private class AuthMailService(
     val authMailRepository: AuthMailRepository
 ): AuthMailUseCase {
     private val random = kotlin.random.Random
-    private val verificationStorage = ConcurrentHashMap<String, String>()
 
-    override fun sendCodeToEmail(authMailDTO: AuthMailDTO): VerifyCodeDTO {
+    override fun sendCodeToEmail(authMailDTO: AuthMailDTO): Mono<VerifyCodeDTO> {
         val verifyCode = (100000..999999).random(random).toString()
-        verificationStorage[authMailDTO.email] = verifyCode
-        return VerifyCodeDTO(authMailDTO.email, verifyCode)
+        val verifyCodeDTO = VerifyCodeDTO(authMailDTO.email, verifyCode)
+
+        return authMailRepository.save(verifyCodeDTO)
     }
 
     override fun verifyCode(verifyCodeDTO: VerifyCodeDTO): Mono<Boolean> {
-        val storedCode = verificationStorage[verifyCodeDTO.email]
-        val isMatch = storedCode == verifyCodeDTO.verifyCode
-        return Mono.just(isMatch)
+        val storedCode = authMailRepository.findByEmail(verifyCodeDTO.email)
+
+        return if (storedCode == verifyCodeDTO.verifyCode) {
+            authMailRepository.delete(verifyCodeDTO.email)
+            Mono.just(true)
+        } else {
+            Mono.just(false)
+        }
     }
 }
