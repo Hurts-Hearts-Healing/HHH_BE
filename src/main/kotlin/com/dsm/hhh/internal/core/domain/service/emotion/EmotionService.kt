aa1f56da7355ab81efae73f483265f54fb3344f9
seung won 🏆 <tmddnjsrla99@gmail.com>
2025-05-22 19:09:04 +0900

    feat: 하루에 한 개의 감정 이모지를 선택할 수 있도록 변경

--- src/main/kotlin/com/dsm/hhh/internal/core/domain/service/emotion/EmotionService.kt
+++ src/main/kotlin/com/dsm/hhh/internal/core/domain/service/emotion/EmotionService.kt
@@ -4,37 +4,46 @@ import com.dsm.hhh.external.error.ErrorCode
 import com.dsm.hhh.internal.common.exception.CustomExceptionFactory
 import com.dsm.hhh.internal.core.domain.component.user.CurrentUser
 import com.dsm.hhh.internal.core.domain.model.dto.emotion.EmotionInternalDTO
+import com.dsm.hhh.internal.core.domain.model.primitive.diary.CreatedAt
 import com.dsm.hhh.internal.core.usecase.emotion.EmotionUseCase
 import com.dsm.hhh.internal.data.repository.emotion.EmotionRepository
-import org.springframework.dao.DuplicateKeyException
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
-
 @Service
-private class EmotionRegisterService(
+private class EmotionService(
     private val emotionRepository: EmotionRepository,
     private val currentUser: CurrentUser
 ) : EmotionUseCase {
+
     override fun register(emotionInternalDTO: EmotionInternalDTO): Mono<Void> {
-        return emotionRepository.save(emotionInternalDTO)
-            .onErrorResume {
-                when (it) {
-                    is DuplicateKeyException -> throw CustomExceptionFactory.conflict(ErrorCode.AUTH_004)
-                    is DuplicateKeyException -> throw CustomExceptionFactory.serviceUnavailable(ErrorCode.INTERNAL_001)
-                    else -> throw CustomExceptionFactory.internalServerError(ErrorCode.INTERNAL_001)
+        return currentUser.get()
+            .doOnNext { user -> emotionInternalDTO.userId = user.userId }
+            .flatMap {
+                val userId = emotionInternalDTO.userId
+                    ?: return@flatMap Mono.error(CustomExceptionFactory.unauthorized(ErrorCode.AUTH_005))
+
+                emotionRepository.findByUserIdAndCreatedAt(userId, CreatedAt.now())
+                    .hasElement()
+            }
+            .flatMap { exists ->
+                if (exists) {
+                    Mono.error(CustomExceptionFactory.conflict(ErrorCode.EMOTION_001))
+                } else {
+                    emotionRepository.save(emotionInternalDTO)
                 }
             }
-            .then()
     }
 
     override fun findByUserId(): Flux<EmotionInternalDTO> {
         return currentUser.get()
             .flatMapMany { user ->
-                emotionRepository.findByUserId(
-                    user.userId
-                        ?: return@flatMapMany Flux.error(CustomExceptionFactory.unauthorized(ErrorCode.AUTH_003))
-                )}
+                val userId = user.userId
+                    ?: return@flatMapMany Flux.error(CustomExceptionFactory.unauthorized(ErrorCode.AUTH_003))
+
+                emotionRepository.findByUserId(userId)
+            }
     }
+
 }
