29ad5fe16d982dd0dc3d20b1f7e216f7f39adaea
seung won 🏆 <tmddnjsrla99@gmail.com>
2025-05-22 19:08:44 +0900

    fix: Entity Type 올바르게 변경
    CreatedAt? -> LocalDate

--- src/main/kotlin/com/dsm/hhh/internal/data/repository/emotion/EmotionEntity.kt
+++ src/main/kotlin/com/dsm/hhh/internal/data/repository/emotion/EmotionEntity.kt
@@ -1,12 +1,9 @@
 package com.dsm.hhh.internal.data.repository.emotion
 
-import com.dsm.hhh.internal.core.domain.model.primitive.diary.CreatedAt
-import com.dsm.hhh.internal.core.domain.model.primitive.user.UserId
 import com.dsm.hhh.internal.data.repository.CollectionSpec
-import com.dsm.hhh.internal.data.repository.user.UserEntity
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
-
+import java.time.LocalDate
 
 @Document(collation = CollectionSpec.EMOTION)
 class EmotionEntity private constructor(
@@ -17,10 +14,12 @@ class EmotionEntity private constructor(
 
     val emotion: String,
 
-    val createdAt: CreatedAt?
+    val createdAt: LocalDate
 ){
+
     constructor(
         userId: String,
         emotion: String
-    ):this(null, userId, emotion, null)
+    ):this(null, userId, emotion, LocalDate.now())
+
 }
