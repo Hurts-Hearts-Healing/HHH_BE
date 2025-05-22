be5ff77aa9c2e2c64bff952798085fed1bfe06ba
seung won 🏆 <tmddnjsrla99@gmail.com>
2025-05-22 19:07:32 +0900

    refactor: Domain Primitive 적용

--- src/main/kotlin/com/dsm/hhh/external/web/rest/emotion/form/EmotionRequestForm.kt
+++ src/main/kotlin/com/dsm/hhh/external/web/rest/emotion/form/EmotionRequestForm.kt
@@ -1,6 +1,7 @@
 package com.dsm.hhh.external.web.rest.emotion.form
 
+import com.dsm.hhh.internal.core.domain.model.primitive.emotion.Emotion
+
 data class EmotionRequestForm(
-    val userId: String,
-    val emotion: String
+    val emotion: Emotion
 )
