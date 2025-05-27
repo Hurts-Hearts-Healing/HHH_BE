3320b063c9fcd9bea829fdc4232bcbf9226c4b0c
eeuunn <haspring@dsm.hs.kr>
2025-05-27 14:58:07 +0900

    .

---
+++ src/main/kotlin/com/dsm/hhh/internal/data/repository/mail/AuthMailRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package com.dsm.hhh.internal.data.repository.mail
+
+import com.dsm.hhh.internal.core.domain.model.dto.mail.AuthMailDTO
+import com.dsm.hhh.internal.core.domain.model.dto.mail.VerifyCodeDTO
+import org.springframework.stereotype.Repository
+import reactor.core.publisher.Mono
+import java.util.concurrent.ConcurrentHashMap
+
+@Repository
+class AuthMailRepositoryImpl(
+): AuthMailRepository {
+    private val database: ConcurrentHashMap<String, String> = ConcurrentHashMap()
+
+    override fun save(verifyCodeDTO: VerifyCodeDTO): Mono<VerifyCodeDTO> {
+        database[verifyCodeDTO.email] = verifyCodeDTO.verifyCode
+        return Mono.just(verifyCodeDTO)
+    }
+
+    override fun findByEmail(email: String): String? {
+        return database[email]
+    }
+
+    override fun delete(email: String) {
+        database.remove(email)
+    }
+}
