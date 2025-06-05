b65b39aaf0525f9f0f3a22225ff9656215df0daa
eeuunn <haspring@dsm.hs.kr>
2025-06-05 11:14:06 +0900

    전체 조회로 수정 repository

--- src/main/kotlin/com/dsm/hhh/internal/data/repository/emotion/analysis/EmotionAnalysisMongoRepository.kt
+++ src/main/kotlin/com/dsm/hhh/internal/data/repository/emotion/analysis/EmotionAnalysisMongoRepository.kt
@@ -1,13 +1,8 @@
 package com.dsm.hhh.internal.data.repository.emotion.analysis
 
-import com.dsm.hhh.external.web.rest.emotion.analysis.response.EmotionAnalysisResponse
-import com.dsm.hhh.internal.core.domain.model.primitive.diary.DiaryId
-import com.dsm.hhh.internal.core.domain.model.primitive.user.UserId
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
-import reactor.core.publisher.Mono
+import reactor.core.publisher.Flux
 
 interface EmotionAnalysisMongoRepository : ReactiveMongoRepository<EmotionAnalysisEntity, String> {
-
-    fun findByUserIdAndDiaryId(userId: String, diaryId: String): Mono<EmotionAnalysisEntity>
-
+    fun findByUserId(userId: String): Flux<EmotionAnalysisEntity>
 }
--- src/main/kotlin/com/dsm/hhh/internal/data/repository/emotion/analysis/EmotionAnalysisRepositoryImpl.kt
+++ src/main/kotlin/com/dsm/hhh/internal/data/repository/emotion/analysis/EmotionAnalysisRepositoryImpl.kt
@@ -1,8 +1,10 @@
 package com.dsm.hhh.internal.data.repository.emotion.analysis
 
 import com.dsm.hhh.internal.core.domain.model.dto.emotion.analysis.EmotionAnalysisInternalDTO
+import com.dsm.hhh.internal.core.domain.model.primitive.user.UserId
 import com.dsm.hhh.internal.data.repository.emotion.analysis.mapper.EmotionAnalysisEntityMapper
 import org.springframework.stereotype.Repository
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @Repository
@@ -15,8 +17,8 @@ class EmotionAnalysisRepositoryImpl(
         return emotionAnalysisMongoRepository.save(entity).then()
     }
 
-    override fun findById(diaryId: String): Mono<EmotionAnalysisInternalDTO> {
-        return emotionAnalysisMongoRepository.findById(diaryId)
+    override fun findByUserId(userId: UserId): Flux<EmotionAnalysisInternalDTO> {
+        return emotionAnalysisMongoRepository.findByUserId(userId.value())
             .map(EmotionAnalysisEntityMapper::toDto)
     }
 }
