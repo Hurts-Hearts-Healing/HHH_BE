ff81122745cc6b9a267b9955d1416aa0955cf1aa
seung won 🏆 <tmddnjsrla99@gmail.com>
2025-06-23 15:22:01 +0900

    refactor: web client 요청 분리

--- src/main/kotlin/com/dsm/hhh/internal/core/client/GeminiClientImpl.kt
+++ src/main/kotlin/com/dsm/hhh/internal/core/client/GeminiClientImpl.kt
@@ -1,17 +1,19 @@
 package com.dsm.hhh.internal.core.client
 
+import com.dsm.hhh.external.web.client.WebClientRequest
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
-import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
-import org.springframework.http.MediaType
 
 @Component
 class GeminiClientImpl(
-    private val webClient: WebClient,
+    private val webClientRequest: WebClientRequest,
 
     @Value("\${google.gemini.api.key}")
-    private val apiKey: String
+    private val apiKey: String,
+
+    @Value("\${google.gemini.api.uri}")
+    private val apiUri: String
 ) : GeminiClient {
 
     override fun analyzeEmotion(text: String): Mono<String> {
@@ -25,12 +27,7 @@ class GeminiClientImpl(
             )
         )
 
-        return webClient.post()
-            .uri("https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash:generateContent?key=$apiKey")
-            .contentType(MediaType.APPLICATION_JSON)
-            .bodyValue(requestBody)
-            .retrieve()
-            .bodyToMono(Map::class.java)
+        return webClientRequest.post(apiUri + apiKey, requestBody)
             .map { response ->
                 val candidates = response["candidates"] as? List<*> ?: return@map "감정 분석 실패"
                 val first = candidates.firstOrNull() as? Map<*, *>
